c613cfafc56bac76aea0bee101df8b2a0284e0bd
marcfreir <marcfreir@outlook.com>
2021-01-28 09:59:21 -0200

    Initial commit Add __init__.py & app.py

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+
+root = Tk
+root.title('WiNote')
+# root.iconbitmap('../_img/winote.ico')
+root.geometry('1200x660')
+
+
+root.mainloop()
